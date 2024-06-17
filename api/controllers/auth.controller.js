dc6e07aee4206b33c8b9ea25d527727bd6112248
Hamidaamir <hamidamir021@gmail.com>
2024-06-18 01:33:27 +0500

    update header and made profile page private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,7 +19,7 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User nor found!"));
+    if (!validUser) return next(errorHandler(404, "User not found!"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
@@ -33,7 +33,7 @@ export const signin = async (req, res, next) => {
   }
 };
 
-export const google = async (res, req, next) => {
+export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
@@ -48,7 +48,7 @@ export const google = async (res, req, next) => {
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-      const newUser = newUser({
+      const newUser = new User({
         username:
           req.body.name.split(" ").join("").toLowerCase() +
           Math.random().toString(36).slice(-4),
